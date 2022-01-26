81dd31b9f356654c59288c617883a4cde0e80ccb
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2022-01-26 15:25:34 +0100

    Initial commit

---
+++ Commander.NET/Commander.cs
@@ -0,0 +1,71 @@
+﻿namespace Commander_Net;
+
+public class Command
+{
+    internal string[] Identifiers { get; }
+
+    internal readonly Action<string> method;
+    internal readonly List<Command> subCommands = new();
+
+    public Command(Action<string> method, string[] identifiers)
+    {
+        this.method = method;
+        Identifiers = identifiers;
+    }
+
+    public Command Register(Action<string> method, params string[] identifiers)
+    {
+        Command command = new Command(method, identifiers);
+        subCommands.Add(command);
+        return command;
+    }
+
+    internal void Execute(string input)
+    {
+        string[] inputParts = input.Split(' ');
+        bool found = false;
+
+        foreach (Command? command in subCommands)
+        {
+            if (command.Identifiers.Contains(inputParts[0]))
+            {
+                command.Execute(string.Join(' ', inputParts.Skip(1)));
+                found = true;
+            }
+        }
+
+        if (!found)
+        {
+            method.Invoke(input);
+        }
+    }
+}
+
+public class Commander
+{
+    private readonly List<Command> commands = new();
+
+    public Command Register(Action<string> method, params string[] identifiers)
+    {
+        Command command = new Command(method, identifiers);
+        commands.Add(command);
+        return command;
+    }
+
+    public bool Execute(string input)
+    {
+        string[] inputParts = input.Split(' ');
+        bool found = false;
+
+        foreach (Command? command in commands)
+        {
+            if (command.Identifiers.Contains(inputParts[0]))
+            {
+                command.Execute(string.Join(' ', inputParts.Skip(1)));
+                found = true;
+            }
+        }
+
+        return found;
+    }
+}
---
+++ Commander.Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using Commander_Net;
+
+Console.WriteLine("Hello, World!");
+
+Commander commander = new Commander();
+Command sayCommand = commander.Register(HahaYes, "say", "tell");
+sayCommand.Register(HahaYesSub, "kek");
+
+commander.Register(HahaNo, "scream", "yell");
+commander.Register((args) => Console.Clear(), "clear");
+
+while (true)
+{
+    if (!commander.Execute(Console.ReadLine()!))
+    {
+        Console.WriteLine("Command not found!");
+    }
+}
+
+void HahaYes(string yes)
+{
+    Console.WriteLine($">>> {yes}");
+}
+
+void HahaYesSub(string yes)
+{
+    Console.WriteLine($">>> {yes} xD");
+}
+
+void HahaNo(string no)
+{
+    Console.WriteLine($">>> {no.ToUpper()}");
+}
